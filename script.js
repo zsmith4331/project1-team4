45aa48a4f2d98a032a7b0aa2a70d4c250f49c8dc
panther <jackfunck21@gmail.com>
2021-01-13 20:09:47 -0500

    local storage mthods

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// save button function to local storage 
+/*function saveEvent (event){
+    var userInput = $(this).siblings("input").val().trim();
+    localStorage.setItem(userInput);
+}
+$(".saveBtn").on("click", saveEvent);
+*/
+// Lists the cities, state, and/or zipcode from local storage.
+for(var i = 0; i < localStorage.length; i++){
+    var getItem = $("").addClass("");
+    locName.append("<li>"+ Item + "</li>");
+}
+
+var localS =localStorage.setItem();
