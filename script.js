cf61339c2377efd242cf966e9dfe73b2cc05e855
panther <jackfunck21@gmail.com>
2021-01-16 08:02:49 -0500

    working on city list still trying to get working

--- script.js
+++ script.js
@@ -7,18 +7,38 @@
 $(".saveBtn").on("click", saveEvent);
 */
 // Lists the cities, state, and/or zipcode from local storage.
+
+$(document).ready(function(){
 var search = $("#search");
 var searchBtn = $("#button");
 
 searchBtn.on("click", function(event){
 
-for(var i = 0; i < localStorage.length; i++){
-    var cityN = localStorage.getItem(i);
-    var locName = $("#cityList").addClass("cityL");
-    locName.append("<li>"+ cityName + "</li>");
+ 
+function displayCity(){
+    $("#cityList").empty();
+    var data = JSON.parse(localStorage.getItem("cities")) || [];
+
+    //pulling and listing city name
+for(var i = 0; i < data.length; i++){
+    //var cityN = localStorage.getItem(i);
+
+    var cityName = $("#cityList").addClass("cityL");
+
+    cityName.append("<button data-city="+data[i]+" class='cityBtn'>" + data[i] + "</button>" + "<br>");
 }
 
 
+
+
+
+var citInput = $("#search").val();
+var data = JSON.parse(localStorage.getItem("cities")) || [];
+data.push(citInput);
+localStorage.setItem("cities", JSON.stringify(data));
+weatherData(citInput);
+$("#search").val("");
+
 });
 
-/*var localS =localStorage.setItem();*/
+});
