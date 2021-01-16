5884fc9adab949bbc3c157db5f1519964c79e925
Kate Milano <catherine.ann.milano@gmail.com>
2021-01-16 11:25:38 -0500

    create covid file

--- script.js
+++ script.js
@@ -18,26 +18,6 @@ for(var i = 0; i < localStorage.length; i++){
     locName.append("<li>"+ cityN + "</li>");
 }
 
-//https://api.covidtracking.com/v1/us/current.json
-// covid tracking
-var state = $("#searchState").val();
-  var stateFormatted = state.replaceAll(" ", "").toLowerCase();
-  var queryURL5day = "https://api.covidtracking.com/v1/states/" + stateFormatted + "/current.json"
-
-  $.ajax({
-      url: queryURL5day,
-      method: "GET"
-  })
-      // We store all of the retrieved data inside of an object called "response"
-      .then(function(response) {
-        console.log(response)
-        console.log(response.positiveIncrease);
-        console.log(response.positiveCasesViral);
-        console.log(response.hospitalizedCurrently);
-        console.log(response.death);
-        console.log(response.deathIncrease);
-      });
-
 });
 
 /*var localS =localStorage.setItem();*/
