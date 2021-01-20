6eb11eed17fce25422e4c1c84b992e02cbc18cb0
Jack Funck <jackfunck21@gmail.com>
2021-01-20 18:13:46 -0500

    I got the cities to appendwith data names

--- script.js
+++ script.js
@@ -9,44 +9,24 @@ $(".saveBtn").on("click", saveEvent);
 // Lists the cities, state, and/or zipcode from local storage.
 
 
-$(document).ready(function(){
 
-var search = $("#searchCity");
+var search = $("#search");
 var searchBtn = $("#button");
 
 searchBtn.on("click", function(event){
 
-    var citInput = $("#search").val();
+    var citInput = search.val();
     var data = JSON.parse(localStorage.getItem("cities")) || [];
     data.push(citInput);
     localStorage.setItem("cities", JSON.stringify(data));
-    weatherData(citInput);
-    $("#search").val("");
-    
+    console.log(citInput);
+    citySearch(citInput);
  
-function displayCity(){
-    $("#cityList").empty();
-    var data = JSON.parse(localStorage.getItem("cities")) || [];
-
-    //pulling and listing city name
-for(var i = 0; i < data.length; i++){
-    //var cityN = localStorage.getItem(i);
-
-    var cityName = $("#cityList").addClass("cityL");
-
-    cityName.append("<button data-city="+data[i]+" class='cityBtn'>" + data[i] + "</button>" + "<br>");
-
-  for(var i = 0; i < localStorage.length; i++){
-    var cityN = localStorage.getItem(i);
-    var locName = $("#cityList").addClass("cityL");
-    locName.append("<li>"+ cityN + "</li>");
-}
-
 //https://api.covidtracking.com/v1/us/current.json
 // covid tracking
 var state = $("#searchState").val();
-  var stateFormatted = state.replaceAll(" ", "").toLowerCase();
-  var queryURL5day = "https://api.covidtracking.com/v1/states/" + stateFormatted + "/current.json"
+  //var stateFormatted = state.replaceAll(" ", "").toLowerCase();
+  /*var queryURL5day = "https://api.covidtracking.com/v1/states/" + stateFormatted + "/current.json"
 
   $.ajax({
       url: queryURL5day,
@@ -58,21 +38,44 @@ var state = $("#searchState").val();
         console.log(response.hospitalizedCurrently);
         console.log(response.death);
         console.log(response.deathIncrease);
-     
     
+    });*/
+});
+
+
+/*$(document).on("click", ".cityBtn", function (e) {
+    e.preventDefault();
+    console.log("Inside cityBtn");
+    var city = $(this).attr("data-city");
+    console.log("City Name: " + city);
     
-    
-    });
+  });*/
+
+  function displayCity(){
+    $("#cityList").empty();
+    var data = JSON.parse(localStorage.getItem("cities")) || [];
+
+    //pulling and listing city name
+
+    //var cityN = localStorage.getItem(i);
 
 
+  for(var i = 0; i < data.length; i++){
+    //var cityN = localStorage.getItem(i);
+    var locName = $("#cityList").addClass("cityL");
+    locName.append("<button  class='cityPush' data-name='"+data[i]+"'>"+ data[i] + "</button>");
+}
+  }
+displayCity();
 
 
-
-};
-};
+$(".cityPush").on("click", function(){
+var c = $(this).attr("data-name");
+citySearch(c);
 
 
 });
 
-/*var localS =localStorage.setItem();*/
-});
+
+
+
