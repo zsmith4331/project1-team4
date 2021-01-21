0ea2a28de32562b7728330908789fa18b221e6b3
Jack Funck <jackfunck21@gmail.com>
2021-01-20 19:30:36 -0500

    got the clear button working

--- script.js
+++ script.js
@@ -22,34 +22,11 @@ searchBtn.on("click", function(event){
     console.log(citInput);
     citySearch(citInput);
  
-//https://api.covidtracking.com/v1/us/current.json
-// covid tracking
-var state = $("#searchState").val();
-  //var stateFormatted = state.replaceAll(" ", "").toLowerCase();
-  /*var queryURL5day = "https://api.covidtracking.com/v1/states/" + stateFormatted + "/current.json"
 
-  $.ajax({
-      url: queryURL5day,
-      method: "GET"
-  }).then(function(response) {
-        console.log(response)
-        console.log(response.positiveIncrease);
-        console.log(response.positiveCasesViral);
-        console.log(response.hospitalizedCurrently);
-        console.log(response.death);
-        console.log(response.deathIncrease);
-    
-    });*/
 });
 
 
-/*$(document).on("click", ".cityBtn", function (e) {
-    e.preventDefault();
-    console.log("Inside cityBtn");
-    var city = $(this).attr("data-city");
-    console.log("City Name: " + city);
-    
-  });*/
+
 
   function displayCity(){
     $("#cityList").empty();
@@ -73,6 +50,12 @@ $(".cityPush").on("click", function(){
 var c = $(this).attr("data-name");
 citySearch(c);
 
+
+});
+
+$("#clear").on("click", function(){
+$("#cityList").empty();
+localStorage.clear();
 
 });
 
